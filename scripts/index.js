3518dce229a4612b3d87c40591936a64364ceaa2
xRekTzzJ <pavel2003sbk@yandex.ru>
2023-02-16 15:43:02 +0800

    fix: исправил замечания после ревью.

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,16 @@
 let popup = document.querySelector('.popup');
 let editButton = document.querySelector('.profile__edit-button');
 let closeButton = document.querySelector('.popup__close-icon');
+let userName = document.querySelector('#userName');
+let userOccupation = document.querySelector('#userOccupation');
+let formElement = document.querySelector('.popup__form');
+let nameInput = document.querySelector('#userNameInput');
+let occupationInput = document.querySelector('#userOccupationInput');
 //* Открытие едит попапа
 function openEditPopup() {
   popup.classList.add('popup_opened');
+  nameInput.value = userName.textContent;
+  occupationInput.value = userOccupation.textContent;
 }
 editButton.addEventListener('click', openEditPopup);
 //* Закрытие едит попапа
@@ -12,19 +19,10 @@ function closeEditPopup() {
 }
 closeButton.addEventListener('click', closeEditPopup);
 //* Изменение имени и статуса пользователя через формы
-let userName = document.querySelector('#userName');
-let userOccupation = document.querySelector('#userOccupation');
-userName.textContent = 'Жак-Ив Кусто';
-userOccupation.textContent = 'Исследователь морей';
-let formElement = document.querySelector('.popup__form');
-let nameInput = document.querySelector('#userNameInput');
-let OccupationInput = document.querySelector('#userOccupationInput');
-nameInput.value = userName.textContent;
-OccupationInput.value = userOccupation.textContent;
 function handleFormSubmit(evt) {
   evt.preventDefault();
   userName.textContent = nameInput.value;
-  userOccupation.textContent = OccupationInput.value;
+  userOccupation.textContent = occupationInput.value;
   closeEditPopup();
 }
 formElement.addEventListener('submit', handleFormSubmit);
